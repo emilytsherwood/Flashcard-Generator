4886c6c0bb0a14774c033a0169374074d499d3b1
Emily Turner-Sherwood <emilyturnerr@gmail.com>
2016-12-25 14:13:32 -0800

    Prompt Working

--- basic.js
+++ basic.js
@@ -1,4 +1,28 @@
-var inquirer = require('inquirer');
+var prompt = require('prompt');
+
+function Basic(question, answer){
+	this.question = question;
+	this.answer = answer;
+}
+
+prompt.start();
+
+prompt.get(['question', 'answer'], function(err, result) {
+	console.log('question: ' + result.question);
+	console.log('answer: ' + result.answer);
+});
+
+// Basic.prototype.getBasic = function(){
+// 	return this.question;
+// };
+
+// var firstQuestion = new Basic('');
+// console.log(firstQuestion.getBasic());
+
+
+// var inquirer = require('inquirer');
+
+
 
 // function BasicFlashcard(question, answer){
 // 	this.question = question,
@@ -10,23 +34,23 @@ var inquirer = require('inquirer');
 // };
 
 // var askQuestion = function(){
-	inquirer.prompt([
-	{
-		type: 'input',
-		name: 'question_one',
-		message: 'What animal is on the Canadian quarter?',
-	},
-	{
-		type: 'input',
-		name: 'question_two',
-		message: 'What is the most recent territory in Canada called?',
-	},
-	{
-		type: 'input',
-		name: 'question_three',
-		message: 'How many national languages does Canada have?',
-	}]).then(function(answers){
+// 	inquirer.prompt([
+// 	{
+// 		type: 'input',
+// 		name: 'question_one',
+// 		message: firstQuestion,
+// 	},
+// 	{
+// 	// 	type: 'input',
+// 	// 	name: 'question_two',
+// 	// 	message: 'What is the most recent territory in Canada called?',
+// 	// },
+// 	// {
+// 	// 	type: 'input',
+// 	// 	name: 'question_three',
+// 	// 	message: 'How many national languages does Canada have?',
+// 	// }]).then(function(answers){
+// }]);
+// };
 
-	// });
-});
 
